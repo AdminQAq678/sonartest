9866fa79c661c341bc5323b5e8e8f00247859260
IIIIIIIIIIII <2549793997@qq.com>
2021-10-22 18:08:11 +0800

    sonar sett

--- my.py
+++ my.py
@@ -1,3 +1,19 @@
 async def myfucn():
     return
     pass
+
+
+def myfunc1():
+
+    pass
+    print('asdsadsadasadsa')
+    print('asdsadsadasadsa')
+
+    print('asdsadsadasadsa')
+    print('asdsadsadasadsa')
+
+    print('asdsadsadasadsa')
+    print('asdsadsadasadsa')
+
+    print('asdsadsadasadsa')
+    print('asdsadsadasadsa')
